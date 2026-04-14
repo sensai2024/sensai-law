d17efef2ecf38b4e9a9543ca4e8e43a0a689f3c0
seifelshemy21 <seifelshemy1@gmail.com>
2026-04-14 17:35:12 +0200

    another fix

--- src/features/auth/AuthContext.jsx
+++ src/features/auth/AuthContext.jsx
@@ -78,6 +78,7 @@ export const AuthProvider = ({ children }) => {
   );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (context === undefined) {
