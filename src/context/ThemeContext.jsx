eba1dbd7a7651c4bbb1fd3ad3a8f8a02dd00c5a8
seifelshemy21 <seifelshemy1@gmail.com>
2026-04-27 22:41:09 +0300

    fix error

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -38,6 +38,7 @@ export const ThemeProvider = ({ children }) => {
   );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useTheme = () => {
   const context = useContext(ThemeContext);
   if (context === undefined) {
